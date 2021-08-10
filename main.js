8abdcc977da164b3db0567ffb04ada7923b185d8
fgikh167 <88747264+fgikh167@users.noreply.github.com>
2021-08-10 18:20:29 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require('electron')
+const preload = require('./preload')
+function mostrarJanela() {
+    const janela = new BrowserWindow({
+        width: 1280,
+        height: 800,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+    janela.loadURL('https://krunker.io');
+    janela.once("ready-to-show", ()=>{
+        janela.show()
+    })
+    janela.setMenuBarVisibility(false)
+}
+app.whenReady().then(mostrarJanela)
+
+let scripts = [() => {
+    (require("./Scripts/script.js"));
+}]
+const runScripts = () => { 
+    scripts.forEach(script => {
+        (script)()
+    }); 
+}
+
+runScripts()
+
+
+
